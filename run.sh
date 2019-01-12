e2ab4769f078e7d438077ab33815567190aadf42
FacuM <facumo.fm@gmail.com>
2019-01-11 21:21:31 -0300

    Report "Waiting for updates..." ONLY after a request.

--- run.sh
+++ run.sh
@@ -29,6 +29,7 @@ function getSite
  while [ "$STATUS" != 'Ok' ]
  do
   LINK=${links[ $(( $RANDOM % ${#links[@]} )) ]}
+  echo "$SPOKEN_NAME"' is checking the link he found...'
   sendMessage "$SPOKEN_NAME"' is checking the link he found...'
   CURL_TRY=$(curl -w '\n%{time_total}' $LINK -s)
   CURL_STATUS=$?
@@ -39,13 +40,17 @@ function getSite
   fi
   if [ $CURL_STATUS -ne 0 ] || [ "$DOMAIN_REGISTRATION" == 'unregistered' ]
   then
+   echo "$SPOKEN_NAME"" found out it's down. No fun for ""$LINK"'. Will now try again...'
    sendMessage "$SPOKEN_NAME"" found out it's down. No fun for ""$LINK"'. Will now try again...'
   else
    STATUS='Ok'
   fi
  done
+ echo "$SPOKEN_NAME"' is happy, this one took just '"$RESPONSE_TIME"' seconds to reply.'"
+$LINK"
  sendMessage "$SPOKEN_NAME"' is happy, this one took just '"$RESPONSE_TIME"' seconds to reply.'"
 $LINK"
+ printf "$DATESTR"'Waiting for updates...'
 }
 
 # Main
@@ -59,18 +64,19 @@ else
 fi
 LAST=""
 COUNT=0
-printf '\n'
+printf '\n'"$(date '+%d-%m-%y / %H:%M')"': Say hi to '"$SPOKEN_NAME""! I'm up and running.\n"
 while true
 do
  LAST=$(curl -s "https://api.telegram.org/bot""$API_KEY""/getUpdates" -F offset=$(( $LUID + 1 )))
  LCID=$(echo "$LAST" | tail -1 | sed 's/.*chat":{"id"://g' | cut -d ',' -f 1)
  LMSG=$(echo "$LAST" | sed 's/text/\ntext/' | tail -1 | cut -d '"' -f 3)
- printf "$(date '+%d-%m-%y / %H:%M')"': '
+ OUTPUT="$OUTPUT""$(date '+%d-%m-%y / %H:%M')"': '
+ DATESTR="$OUTPUT"
  LUID=$(echo "$LAST" | grep update_id)
  if [ "$?" -eq 0 ]
  then
   LUID=$(echo "$LUID" | head -1 | cut -d ':' -f 4 | cut -d ',' -f 1)
-  printf 'Update #'"$(( $LUID + 1 ))"' from chat ID '"$LCID"'.'
+  printf "$OUTPUT"'Update #'"$(( $LUID + 1 ))"' from chat ID '"$LCID"'.\n'
   case "$LMSG" in
 	'/getsite')
 	  getSite &
@@ -80,10 +86,8 @@ do
 	  getSite &
 	  ;;
   esac
- else
-  printf "no updates found."
  fi
- printf '\n'
+ OUTPUT='\n'
  COUNT=$(( $COUNT + 1 ))
  #printf '\rRan '"$COUNT"' times'
 done
